30a5f089d05432285d90738ff65f8cd8ee8552f5
José Anderson <joseandersonsilv4@gmail.com>
2023-02-13 23:48:42 -0300

    Add class Exibir_Agenda

---
+++ src/entities/ExibirAgenda.java
@@ -0,0 +1,22 @@
+package entities;
+
+import java.util.List;
+
+public class ExibirAgenda {
+    
+    public static void agenda_por_procedimento(List<Appointment> list_consultas, int dia, int mes, int ano){
+        for(int i = 0; i < list_consultas.size(); i++){
+            if(list_consultas.get(i).dia == dia && list_consultas.get(i).mes == mes && list_consultas.get(i).ano == ano)
+                System.out.printf("%d/%d/%d -> Procedimento: %s/n", dia, mes, ano, list_consultas.get(i).servico.procedimento);
+            
+        }
+
+    }
+    public static void agenda_por_profissional(List<Appointment> list_consultas, int dia, int mes, int ano){
+        for(int i = 0; i < list_consultas.size(); i++){
+            if(list_consultas.get(i).dia == dia && list_consultas.get(i).mes == mes && list_consultas.get(i).ano == ano)
+                System.out.printf("%d/%d/%d - %d:%d -> Procedimento: %s/n", dia, mes, ano, list_consultas.get(i).medico.nome, list_consultas.get(i).hora, list_consultas.get(i).minuto);
+            
+        }
+    }
+}
