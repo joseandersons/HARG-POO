0904fc878eb784aa884916760020b129293fd079
joseandersons <101954231+joseandersons@users.noreply.github.com>
2023-02-13 23:55:40 -0300

    Update ExibirAgenda.java
    Retificando Add class Exibir_Agenda

--- src/entities/ExibirAgenda.java
+++ src/entities/ExibirAgenda.java
@@ -7,7 +7,7 @@ public class ExibirAgenda {
     public static void agenda_por_procedimento(List<Appointment> list_consultas, int dia, int mes, int ano){
         for(int i = 0; i < list_consultas.size(); i++){
             if(list_consultas.get(i).dia == dia && list_consultas.get(i).mes == mes && list_consultas.get(i).ano == ano)
-                System.out.printf("%d/%d/%d -> Procedimento: %s/n", dia, mes, ano, list_consultas.get(i).servico.procedimento);
+                System.out.printf("%d/%d/%d -> Procedimento: %s/n", dia, mes, ano, list_consultas.get(i).nomeServico);
             
         }
 
@@ -15,7 +15,7 @@ public class ExibirAgenda {
     public static void agenda_por_profissional(List<Appointment> list_consultas, int dia, int mes, int ano){
         for(int i = 0; i < list_consultas.size(); i++){
             if(list_consultas.get(i).dia == dia && list_consultas.get(i).mes == mes && list_consultas.get(i).ano == ano)
-                System.out.printf("%d/%d/%d - %d:%d -> Procedimento: %s/n", dia, mes, ano, list_consultas.get(i).medico.nome, list_consultas.get(i).hora, list_consultas.get(i).minuto);
+                System.out.printf("%d/%d/%d - %d:%d -> Procedimento: %s/n", dia, mes, ano, list_consultas.get(i).nomeMedico, list_consultas.get(i).hora, list_consultas.get(i).minuto);
             
         }
     }
