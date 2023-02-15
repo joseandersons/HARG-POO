56b370baffe485c978ebd272bf14ad91d38c3a5d
Guilherme Gomes <guilherme.o.s.g21@gmail.com>
2023-02-14 21:09:15 -0300

    Consertando a classe Caixa

--- src/entities/Caixa.java
+++ src/entities/Caixa.java
@@ -9,14 +9,15 @@ public class Caixa {
         this.caixa = 0;
     }
 
-    public double addCaixa(List<Orcamento> listaOrcamento) {
-        for(int i = 0; i < listaOrcamento.size(); i++)
-            caixa += listaOrcamento.get(i).valor;
-        
-        return caixa;
+    public void addCaixa(Orcamento orcamento) {
+        caixa += orcamento.valor;
     }
 
     public void clearList(List<Orcamento> listaOrcamento) {
+        for(int i = 0; i < listaOrcamento.size(); i++)
+            System.out.printf("Orçamento %d: %.2lf\n", (i+1), listaOrcamento.get(i).valor);
+
+        System.out.printf("Total de dinheiro em caixa: %.2lf", caixa);
         listaOrcamento.clear();
     }
 }
