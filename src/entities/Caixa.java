92175b0e3e336c10fa23432ff784af15e4faad71
ritalemos <rklp@ic.ufal.br>
2023-02-15 15:19:57 -0300

    att

--- src/entities/Caixa.java
+++ src/entities/Caixa.java
@@ -15,14 +15,15 @@ public class Caixa {
 
     public void clearList(List<Orcamento> listaOrcamento) {
         for(int i = 0; i < listaOrcamento.size(); i++){
-            System.out.printf("Orçamento %d", (i+1));
+            System.out.printf("Orçamento %d\n", (i+1));
             for(int j = 0; j < listaOrcamento.get(i).procedimento.size(); j++)
                 System.out.printf("Procedimento %s R$%.2f\n", listaOrcamento.get(i).procedimento.get(j).procedimento, listaOrcamento.get(i).procedimento.get(j).valor);
-            System.out.printf("Valor total: %.2f\n", listaOrcamento.get(i).valor);
+            System.out.printf("Valor total: R$%.2f\n", listaOrcamento.get(i).valor);
         }
 
         System.out.println();
         System.out.printf("Total de dinheiro em caixa: %.2f\n", caixa);
         listaOrcamento.clear();
+        this.caixa = 0;
     }
 }
