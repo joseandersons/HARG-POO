2576e09a957a4cbad2c6d771c76b762946a7e187
Higor de Lima <limahigor.g@gmail.com>
2023-02-15 00:02:57 -0300

    update

--- src/entities/Caixa.java
+++ src/entities/Caixa.java
@@ -15,7 +15,7 @@ public class Caixa {
 
     public void clearList(List<Orcamento> listaOrcamento) {
         for(int i = 0; i < listaOrcamento.size(); i++)
-            System.out.printf("Orçamento %d: Procedimento %s R$%.2lf\n", (i+1), listaOrcamento.get(i).procedimento.procedimento, listaOrcamento.get(i).valor);
+            System.out.printf("Orçamento %d: Procedimento %s R$%.2f\n", (i+1), listaOrcamento.get(i).procedimento.procedimento, listaOrcamento.get(i).valor);
 
         System.out.println();
         System.out.printf("Total de dinheiro em caixa: %.2f\n", caixa);
